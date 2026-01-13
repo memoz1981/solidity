e6602967c2f4628d61c43a477a8fa938955608b3
M.Z <zeynalov@gmail.com>
2026-01-13 19:41:27 +0400

    ERC20_1 -> 1, not building

---
+++ 2. Solidity Language/src/Library/ERC20_1.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IERC20} from "./IERC20.sol";
+
+// My first implementation of ERC20 - no AI, just learning, and no need to get it right first time
+// now assuming if the balance has been approved for use for third party -> I can still use it??? 
+// first implementation -> have fun :)
+// Note: normally I prefer _ for private fields...but for the sake of interface consistency using it for parameters instead
+struct Balance
+{
+    uint256 totalBalance;
+    mapping(address => uint256) approvals; 
+    bool exists;
+}
+
+/* My first ERC20 Implementation*/
+abstract contract ERC20_1 is IERC20 {
+    string name; 
+    string symbol; 
+    uint8 decimals; 
+    uint256 totalSupply; 
+    address owner;
+
+    mapping(address => Balance) balanceSheet;
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals)
+    {
+        name = _name; 
+        symbol = _symbol;
+        decimals = _decimals; 
+        owner = msg.sender;
+    }
+    
+    function balanceOf(address _owner) public view returns (uint256 balance)
+    {
+        return balanceSheet[_owner].totalBalance;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success)
+    {
+        // not sure if a check to see if the dictionary contains the value (or zero) is required here...
+        // also when does it need to return false???
+        require(balanceSheet[msg.sender].totalBalance < _value, "Not enough funds...");
+
+        balanceSheet[msg.sender].totalBalance -= _value;
+        if(balanceSheet[msg.sender].totalBalance == 0)
+        {
+            delete balanceSheet[msg.sender];
+        }
+
+        Balance storage to = balanceSheet[_to];
+        to.exists = true;
+        to.totalBalance += _value; 
+
+        emit Transfer(msg.sender, _to, _value);
+        return true; 
+    }
+
+    // since interface definition doesn't specify any reverts, will just return false on validations
+    function approve(address _spender, uint256 _value) public returns (bool success)
+    {
+        if(balanceSheet[msg.sender].totalBalance < _value)
+        {
+            return false;
+        }
+
+        balanceSheet[msg.sender].approvals[_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) external view returns (uint256 remaining)
+    {
+        return balanceSheet[_owner].approvals[_spender];
+    }
+
+    // the interface definition of approval is vauge -> so assuming above approve method is the right one
+    // to approve transfer from functionality
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success)
+    {
+        require(balanceSheet[_from].approvals[msg.sender] >= _value);
+        
+        // again having an ambiguity on if the total balance should be deducted on approvals, or still to use total balance
+        // also not sure if to revert on below or return false. 
+        if(balanceSheet[_from].totalBalance < _value)
+        {
+            return false; 
+        }
+
+        balanceSheet[_from].totalBalance -= _value; 
+        balanceSheet[_from].approvals[msg.sender] -= _value; 
+
+        if(balanceSheet[_from].approvals[msg.sender] == 0)
+        {
+            delete balanceSheet[_from].approvals[msg.sender];
+        }
+
+        emit Transfer(_from, _to, _value);
+    }
+}
--- 2. Solidity Language/src/Library/IERC20.sol
+++ 2. Solidity Language/src/Library/IERC20.sol
@@ -39,8 +39,7 @@ interface IERC20 {
 
     // Allows _spender to withdraw from your account multiple times, up to the _value amount. 
     // If this function is called again it overwrites the current allowance with _value.
-
-    // Note: To prevent attack vectors like the one described here and discussed here, 
+    // Note: To prevent attack vectors like the one described here and discussed here (where???), 
     // clients SHOULD make sure to create user interfaces in such a way that they set the allowance first to 
     // 0 before setting it to another value for the same spender. THOUGH The contract itself shouldn’t enforce it, 
     // to allow backwards compatibility with contracts deployed before
